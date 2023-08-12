8d363f9c2c9f2a0da4c8df603e84dba64133d4b0
bleyerj <58072043+bleyerj@users.noreply.github.com>
2023-08-12 14:54:50 +0200

    Update launch_mealor.sh

--- launch_mealor.sh
+++ launch_mealor.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-docker run --init --rm -ti -p 8888:8888 --name mealor -e JUPYTER_ENABLE_LAB=yes -e CHOWN_HOME=yes -e CHOWN_EXTRAOPTS='-hR' --user root -v "$(pwd)":/home/jovyan/shared mealor:latest
-
+docker run --init --rm -ti -p 8888:8888 --e JUPYTER_ENABLE_LAB=yes -e CHOWN_HOME=yes -e CHOWN_EXTRAOPTS='-hR' --user root -v "$(pwd)":/home/jovyan/shared ghcr.io/bleyerj/mealor:latest
